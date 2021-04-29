7f680e3b01d6976dd746e7a956dca5469a132f6f
Masud Hussain <mhussain790@gmail.com>
2021-04-29 00:31:14 -0700

    Updated code for the file_sum method to only convert numbers to float.

--- file_sum.py
+++ file_sum.py
@@ -8,11 +8,12 @@ Assignment: 5A
 def file_sum(text_file_name):
     total = 0
 
-    # Reads numbers line by line from a file and converts them to int/float to add
+    # Reads numbers line by line from a file and converts them to float
+    # adds the converted float from text file to the total var
     try:
         with open(text_file_name, 'r') as infile:
             for line in infile:
-                total += int(float(line))
+                total += float(line)
     # Except throws an error if the file is not found
     except FileNotFoundError:
         print("The file was not found")
